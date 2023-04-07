2339828b68f026478c6334c1a4474df256244b81
rg-ept <130046649+rg-ept@users.noreply.github.com>
2023-04-07 16:47:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,6 @@ import numpy as np
 chat_id = 42877418
 
 def solution(x: np.array) -> float:
-      err = np.random.normal(loc=-29, scale=np.exp(1), size=x.shape) 
-      v_mean = np.mean(x + err)  
-      return v_mean / 10
+      err = np.random.normal(-29+np.exp(1), x.size) 
+      v_mean = np.mean(x/10+err)  
+      return v_mean
